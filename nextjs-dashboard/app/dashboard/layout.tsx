fa6b182f6fd9286307ef15c86db1d088e55f2e6f
waqarahmed6095 <waqarahmed695@gmail.com>
2025-06-04 11:23:50 +0300

    Update .gitignore to include additional files and directories; modify next.config.ts to enable incremental PPR; change Next.js dependency to canary version in package.json and pnpm-lock.yaml; enhance dashboard layout and invoices page with new components and structure.

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 
 import SideNav from '@/app/ui/dashboard/sidenav';
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -9,3 +11,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
     );
+}
